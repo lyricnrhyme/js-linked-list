3e0390b362b1ea3339422658074d1e1b820cdbfc
Jamie Yamashita <jamieyamashita@Jamies-MacBook-Pro-2.local>
2019-03-06 18:40:56 -1000

    updated

--- linkedList.js
+++ linkedList.js
@@ -4,5 +4,53 @@
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
 function linkedListGenerator(){
+    let head = null;
+    let tail = null;
 
+    function getHead() {
+        return head;
+    }
+
+    function getTail() {
+        return tail;
+    }
+
+    function add(data) {
+        let node = {
+            value: data,
+            next: null
+        }
+
+        if (head === null) {
+            head = node;
+            tail = node;
+        } else {
+            tail.next = node;
+            tail = tail.next;
+        }
+
+        return head;
+    }
+
+    function get(data) {
+        console.log(data, 'data');
+        console.log(head, 'head');
+        return head;
+    }
+
+    function remove() {
+    }
+
+
+    function insert() {
+    }
+
+    return {
+        getHead: getHead,
+        getTail: getTail,
+        add: add,
+        remove: remove,
+        get: get,
+        insert: insert
+    }
 }
